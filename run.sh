afcdf7acaf5a53d2ec962bbe36c9aa73aae31100
qJake <jake@burgy.me>
2021-02-22 23:18:32 -0600

    more script updates

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ echo =====================
 echo 
 echo Preparing...
 
-cd /
 apt-get update -y
 apt-get install git python3 wget python3-pip python3-dev python3-rpi.gpio libsdl-ttf2.0-0 python3-sdl2 -y
 python3 -m pip install flask RPi Mock.GPIO pygame spidev
@@ -15,22 +14,17 @@ python3 -m pip install flask RPi Mock.GPIO pygame spidev
 echo Pulling...
 echo 
 
-cd ~
-
 if [ -d '~/nfc-music-box' ]
 then
     echo Cleaning...
-    echo
-
-    cd nfc-music-box
-    ls -A1 | xargs rm -rf
+    echo    
+    ls ~/nfc-music-box -A1 | xargs rm -rf
     cd ..
-    rmdir nfc-music-box
+    rmdir ~/nfc-music-box
 fi
 
-git clone https://github.com/qJake/NFCMusicBox.git nfc-music-box
-cd nfc-music-box
+git clone https://github.com/qJake/NFCMusicBox.git ~/nfc-music-box
 
 echo Running...
 echo
-python3 main.py
+python3 ~/nfc-music-box/main.py &
