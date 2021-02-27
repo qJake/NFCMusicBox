a66b0ced2f25c0452afeb33bcec59cfd79716745
qJake <jake@burgy.me>
2021-02-27 12:57:16 -0600

    Auto-update?

---
+++ update.sh
@@ -0,0 +1,7 @@
+# TODO: This is pretty aggressive, find a better way to identify just our process.
+sudo pkill -f python3
+
+sudo rm -f ./run.sh
+wget --no-cache -O run.sh https://raw.githubusercontent.com/qJake/NFCMusicBox/main/run.sh
+sudo chmod +x run.sh
+sudo ./run.sh
